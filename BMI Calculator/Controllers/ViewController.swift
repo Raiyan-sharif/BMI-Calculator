f9b3891ae2d8ba824d2ddd25be16f4bb1127555e
Raiyan <raiyan.sharif.1234@gmail.com>
2020-07-13 00:37:24 +0600

    BMI calculation works

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightvalueTextValue: UILabel!
+    @IBOutlet weak var weightvalueTextLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = Float(String(format: "%.2f", sender.value))
+        heightvalueTextValue.text = "\(height!)m"
+        
+        
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = Int(sender.value)
+        weightvalueTextLabel.text = "\(weight)Kg"
+    }
+    
+    @IBAction func calcularPressed(_ sender: UIButton) {
+        let height = Float(String(format: "%.2f", heightSlider.value))!
+        let weight = weightSlider.value
+        let bmi = weight / (height * height)
+        
+        print(Float(String(format: "%.2f",bmi))! )
+        
+    }
 }
 
